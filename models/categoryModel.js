02b4262f97e3ffadd3e519e59327b2e234df3607
aaqib <aaqib@finexcloud.com>
2025-08-14 14:42:46 +0500

    Latest 14 Aug 2025

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -9,7 +9,7 @@ const categorySchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ['income', 'transaction'],
+      enum: ['income', 'expense'],
       required: [true, 'Category type is required'],
     },
   },
